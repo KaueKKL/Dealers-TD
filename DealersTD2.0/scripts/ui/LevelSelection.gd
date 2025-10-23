ba607e56e35331fb5a2eb4fb5efafaa1c92ded01
JONATAS JACKSON GONÇALVES <jonatas.goncalves@catolicasc.edu.br>
2025-10-23 19:18:03 -0300

    feat: ui

--- DealersTD2.0/scripts/ui/LevelSelection.gd
+++ DealersTD2.0/scripts/ui/LevelSelection.gd
@@ -1,7 +1,24 @@
 extends Control
 
-func _on_level_01_button_pressed():
+
+
+func _on_voltar_pressed() -> void:
+	SceneManager.change_scene("res://scenes/main/MainMenu.tscn")
+
+func _on_fase_1_pressed() -> void:
 	SceneManager.change_scene("res://scenes/levels/Level01.tscn")
 
-func _on_back_button_pressed():
-	SceneManager.change_scene("res://scenes/main/MainMenu.tscn")
+func _on_fase_2_pressed() -> void:
+	SceneManager.change_scene("res://scenes/levels/Level02.tscn")
+	
+func _on_fase_3_pressed() -> void:
+	SceneManager.change_scene("res://scenes/levels/Level03.tscn")
+	
+func _on_fase_4_pressed() -> void:
+	SceneManager.change_scene("res://scenes/levels/Level04.tscn")
+	
+func _on_fase_5_pressed() -> void:
+	SceneManager.change_scene("res://scenes/levels/Level05.tscn")
+	
+func _on_fase_6_pressed() -> void:
+	SceneManager.change_scene("res://scenes/levels/Level06.tscn")
